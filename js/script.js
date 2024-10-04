5ec807bd48374ce1209072a33cdff3ddbcda87f1
jcquadros <julliepaper@gmail.com>
2024-10-04 01:30:35 -0300

    remake

--- js/script.js
+++ js/script.js
@@ -32,3 +32,11 @@ function erase() {
 window.onload = function () {
   type();
 };
+window.addEventListener("scroll", function () {
+  var navbar = document.querySelector(".navbar");
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
